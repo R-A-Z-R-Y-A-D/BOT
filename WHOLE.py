1a734d0be90820022ca7cd04d2a80ae41c61fe9d
Razryad <58603028+R-A-Z-R-Y-A-D@users.noreply.github.com>
2021-01-13 19:02:02 +0300

    Update WHOLE.py

--- WHOLE.py
+++ WHOLE.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 def wait_int(str=""):
     print(str)
     while True:
@@ -12,7 +11,6 @@ def wait_int(str=""):
             break
         print("uncorrect syntax")
     return (int(a))
-
 def wait_float(str=""):
     print(str)
     while True:
@@ -22,7 +20,6 @@ def wait_float(str=""):
             return (float(a))
         except ValueError:
             print("uncorrect syntax")
-
 def autorisation():
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > app-root > app-wrapper > div > div.aside.aside_game.ng-tns-c79-0.ng-trigger.ng-trigger-gameSidebarAnimation > div > div > app-inventory > div > div > div.empty__login > a"))).click()
     # ввод логина
@@ -43,56 +40,48 @@ def autorisation():
     WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > app-root > app-wrapper > app-modals-container > app-modal > div > div > div > div > div:nth-child(3)"))).click()
     WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > app-root > app-wrapper > app-modals-container > app-modal > div > div > button"))).click()
     # нажать книпку в наличии
-
 def last_crash():
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > app-root > app-wrapper > div > div.aside.aside_game.ng-tns-c79-0.ng-trigger.ng-trigger-gameSidebarAnimation > div > app-crash-sidebar > div.crash__history.xhistory.xhistory_home > div > div > a:nth-child(1)")))
     savelast=driver.find_element(By.CSS_SELECTOR, "body > app-root > app-wrapper > div > div.aside.aside_game.ng-tns-c79-0.ng-trigger.ng-trigger-gameSidebarAnimation > div > app-crash-sidebar > div.crash__history.xhistory.xhistory_home > div > div > a:nth-child(1)")
     lastcrash=savelast.text
     lastcrash=float(lastcrash[:-1])
     return lastcrash
-
 def last_crash_2():
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > app-root > app-wrapper > div > div.aside.aside_game.ng-tns-c79-0.ng-trigger.ng-trigger-gameSidebarAnimation > div > app-crash-sidebar > div.crash__history.xhistory.xhistory_home > div > div > a:nth-child(2)")))
     saveprelast=driver.find_element(By.CSS_SELECTOR, "body > app-root > app-wrapper > div > div.aside.aside_game.ng-tns-c79-0.ng-trigger.ng-trigger-gameSidebarAnimation > div > app-crash-sidebar > div.crash__history.xhistory.xhistory_home > div > div > a:nth-child(2)")
     prelastcrash=saveprelast.text
     prelastcrash=float(prelastcrash[:-1])
     return prelastcrash
-
 def last_crash_3():
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > app-root > app-wrapper > div > div.aside.aside_game.ng-tns-c79-0.ng-trigger.ng-trigger-gameSidebarAnimation > div > app-crash-sidebar > div.crash__history.xhistory.xhistory_home > div > div > a:nth-child(3)")))
     saveprelast = driver.find_element(By.CSS_SELECTOR, "body > app-root > app-wrapper > div > div.aside.aside_game.ng-tns-c79-0.ng-trigger.ng-trigger-gameSidebarAnimation > div > app-crash-sidebar > div.crash__history.xhistory.xhistory_home > div > div > a:nth-child(3)")
     prelastcrash = saveprelast.text
     prelastcrash = float(prelastcrash[:-1])
     return prelastcrash
-
 def last_crash_4():
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > app-root > app-wrapper > div > div.aside.aside_game.ng-tns-c79-0.ng-trigger.ng-trigger-gameSidebarAnimation > div > app-crash-sidebar > div.crash__history.xhistory.xhistory_home > div > div > a:nth-child(4)")))
     saveprelast = driver.find_element(By.CSS_SELECTOR, "body > app-root > app-wrapper > div > div.aside.aside_game.ng-tns-c79-0.ng-trigger.ng-trigger-gameSidebarAnimation > div > app-crash-sidebar > div.crash__history.xhistory.xhistory_home > div > div > a:nth-child(4)")
     prelastcrash = saveprelast.text
     prelastcrash = float(prelastcrash[:-1])
     return prelastcrash
-
 def last_crash_5():
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > app-root > app-wrapper > div > div.aside.aside_game.ng-tns-c79-0.ng-trigger.ng-trigger-gameSidebarAnimation > div > app-crash-sidebar > div.crash__history.xhistory.xhistory_home > div > div > a:nth-child(5)")))
     saveprelast = driver.find_element(By.CSS_SELECTOR, "body > app-root > app-wrapper > div > div.aside.aside_game.ng-tns-c79-0.ng-trigger.ng-trigger-gameSidebarAnimation > div > app-crash-sidebar > div.crash__history.xhistory.xhistory_home > div > div > a:nth-child(5)")
     prelastcrash = saveprelast.text
     prelastcrash = float(prelastcrash[:-1])
     return prelastcrash
-
 def prex5_last_crash():
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > app-root > app-wrapper > div > div.aside.aside_game.ng-tns-c79-0.ng-trigger.ng-trigger-gameSidebarAnimation > div > app-crash-sidebar > div.crash__history.xhistory.xhistory_home > div > div > a:nth-child(6)")))
     saveprelast = driver.find_element(By.CSS_SELECTOR, "body > app-root > app-wrapper > div > div.aside.aside_game.ng-tns-c79-0.ng-trigger.ng-trigger-gameSidebarAnimation > div > app-crash-sidebar > div.crash__history.xhistory.xhistory_home > div > div > a:nth-child(6)")
     prelastcrash = saveprelast.text
     prelastcrash = float(prelastcrash[:-1])
     return prelastcrash
-
 def click(n):
 
     for i in range (n):
         # выбрать все
         WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > app-root > app-wrapper > div > div.aside.aside_game.ng-tns-c79-0.ng-trigger.ng-trigger-gameSidebarAnimation > div > div > app-inventory > div > div.inventory__head > div.inventory__toggle > label > div"))).click()
         time.sleep(0.2)
-
 def balance():
     savebalance = driver.find_element(By.CSS_SELECTOR, "body > app-root > app-wrapper > div > div.aside.aside_game.ng-tns-c79-0.ng-trigger.ng-trigger-gameSidebarAnimation > div > div > app-inventory > div > div.inventory__head > div.inventory__balance > div.inventory__count.symbol_usd")
     balance = float(savebalance.text)
@@ -100,7 +89,6 @@ def balance():
     vybrano = float(savevybrano.text)
     vsego=round((balance+vybrano), 2)
     return vsego
-
 def zakup(bal, bet, type):
     if type==1:
         # магазин скинов
@@ -134,11 +122,9 @@ def zakup(bal, bet, type):
             # крестик
             WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"body > app-root > app-wrapper > div > div.aside.aside_game.ng-tns-c79-0.ng-trigger.ng-trigger-gameSidebarAnimation > div > div > app-shop > div > div.shop__head > div.shop__close"))).click()
             return 1
-
 def start():
     # начать
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > app-root > app-wrapper > div > div.container.container_main.ng-tns-c79-0.ng-trigger.ng-trigger-containerAnimation > div > app-crash-home > div > div.crash__controller > div.crash__information > div > button"))).click()
-
 
 print("Введите любой символ, если хотите изменить параметры ставок или нажмите Enter, в этом случае будут выбраны последние настроенные параметры:")
 file=open("textfail.txt", 'r')
@@ -161,7 +147,7 @@ default=input()
 if default=="":
     time.sleep(1)
     file=open("textfail.txt", 'r')
-    st1=float(file.readline()); st2=float(file.readline()); st3=float(file.readline()); st4=float(file.readline()); st5=float(file.readline()); type_of_bet=int(file.readline())
+    type_of_bet = int(file.readline());st1=float(file.readline()); st2=float(file.readline()); st3=float(file.readline()); st4=float(file.readline()); st5=float(file.readline())
     file.close()
 else:
     type_of_bet = wait_int("Какой баланс? (введите номер выбраного варианта)\n1. Динамический\n2. Статический")
@@ -194,9 +180,7 @@ else:
     else:
         print("1-й краш - ", st1, "$\n2-й краш - ", st2,"$\n3-й краш - ", st3,"$\n4-й краш - ", st4,"$\n5-й краш - ", st5,"$")
         print("Статический баланс" + "\n")
-
-print("Введите несгораемую сумму:")
-podushka=float(input())
+podushka=wait_float("Введите несгораемую сумму:")
 
 # авторизация
 driver = webdriver.Chrome(executable_path='C:\webdrivers\chromedriver.exe')
@@ -213,17 +197,20 @@ while 1:
             zakup(bal, st1, type_of_bet)
             click(2)
             start()
+            click(1)
         a2 = last_crash_2()
         while a2 >= 1.2:
             a2= last_crash_2()
         a = last_crash()
         if a < 1.2:
             if st2 != 0:
+                print("Двойной", st2)
                 click(1)
                 bal = balance()
                 zakup(bal, st2, type_of_bet)
                 click(2)
                 start()
+                click(1)
             a3 = last_crash_3()
             while a3 >= 1.2:
                 a3 = last_crash_3()
@@ -232,11 +219,13 @@ while 1:
                 if st3 != 0:
                     if st2==1:
                         break
+                    print("Тройной", st3)
                     click(1)
                     bal = balance()
                     zakup(bal, st3, type_of_bet)
                     click(2)
                     start()
+                    click(1)
                 a4 = last_crash_4()
                 while a4 >= 1.2:
                     a4 = last_crash_4()
@@ -245,11 +234,13 @@ while 1:
                     if st4 != 0:
                         if st3 == 1:
                             break
+                        print("Четверной", st4)
                         click(1)
                         bal = balance()
                         zakup(bal, st4, type_of_bet)
                         click(2)
                         start()
+                        click(1)
                     a5 = last_crash_5()
                     while a5 >= 1.2:
                         a5 = last_crash_5()
@@ -264,6 +255,7 @@ while 1:
                             zakup(bal, st5, type_of_bet)
                             click(2)
                             start()
+                            click(1)
                         a5 = prex5_last_crash()
                         while a5 >= 1.2:
                             a5 = prex5_last_crash()
@@ -272,17 +264,12 @@ while 1:
                             print("Шестерной краш")
                             break
                         else:
-                            if st5 != 1: click(1)
-                            else: break
+                            if st5 == 1: break
                     else:
-                        if st4 != 1: click(1)
-                        else: break
+                        if st4 == 1: break
                 else:
-                    if st3 != 1: click(1)
-                    else: break
+                    if st3 == 1: break
             else:
-                if st2 != 1:click(1)
-                else: break
+                if st2 == 1: break
         else:
-            if st1 != 1: click(1)
-            else: break
+            if st1 == 1: break
