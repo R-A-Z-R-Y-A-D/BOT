929c025e56b666dca307037722727dde064679e6
R-A-Z-R-Y-A-D <artemtashit@gmail.com>
2021-03-02 23:28:42 +0300

    настройка functions

--- BOT-main/config.py
+++
@@ -1,9 +0,0 @@
-admins = {
-    'zap': 775274522,
-    'bub': 830130638
-}
-
-tokens = {
-    'zap': '1609896509:AAHFFntL5mCRpbRNwtb9mLwOU7zj7XrIsWo',
-    'bub': '1622405338:AAHkL_FBzftlUe2DYtA-cQQ9JRKSt_qpmCc'
-}
